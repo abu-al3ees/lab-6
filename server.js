c809b2cad28158e02e721ff65b1e179e7ae9eb69
abu-al3ees <malkhazali83@gmail.com>
2021-03-30 10:45:16 +0300

    solve conflicet

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 'use strict';
 
@@ -22,148 +23,175 @@ app.use(cors());
 // routes
 app.get('/location', handelLocationRequest);
 app.get('/weather', handelWeatherRequest);
-app.get('/park',handelParkRequest);
+app.get('/park', handelParkRequest);
 app.get('/*',notFoundHandler);
 
 
+function handelParkRequest(req, response) {
+  try{
+    const url =`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${parkApi}`;
+    superAgent.get(url).then( res => {
+      const park=res.body.data;
+      park.map(element =>{
+        const name=element.name;
+        const address=element.address;
+        const fee=element.fee;
+        const description=element.description;
+        const url=element.url;
+       return new Park(name,address,fee,description,url);
 
+      });
+      response.send(parkArr);
+
+    });
+    response.send(parkArr);
+  }
+  catch (error){
+    response.send(error);
+  }
+
+}
+let parkArr=[];
+
+function Park(name,add,fee,des,url){
+  this.name=name;
+  this.address=add;
+  this.fee=fee;
+  this.description=des;
+  this.url=url;
+  parkArr.push(this);
+
+
+
+}
 
 
 function handelLocationRequest(req, response) {
-    try{
-        let city = req.query.city;
-        const url= `https://us1.locationiq.com/v1/search.php?key=${geo_api_key}&q=${city}&format=json`;
-       // let locationData = require('./data/location.json');
-     
-       
-      superAgent.get(url).then(res =>{
-        let getLocationObject = res.body;
-    let locationObject = new Location(city,getLocationObject.display_name,getLocationObject.lat,getLocationObject.lon);
-    response.send(locationObject);
-      })
-    
-       } catch(error){
-           res.status(500).send('something went wrong ')
-       }
-}
-let  weatherArr=[];
-function handelWeatherRequest(req, response) {
-
-    try{
-       
-        if(weatherArr){
-            weatherArr=[];
-        }
-        // let weatherData = require('./data/ weather.json');
-        // let weather = weatherData.data;
-        const url= `https://api.weatherbit.io/v2.0/current/airquality?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${weatherKey}`;
-
-        superAgent.get(url).then(res =>{
-            let getWe = res.body;
-            weather.map(element =>{
-              return  new Weather(getWe.valid_date,getWe.weather.description);
-
-        })
-       
-        response.send(weatherArr);
-        });
-        //res.send(weatherArr);
-    }
-    catch(error){
-        response.status(500).send(error)
-    }
-function handelLocationRequest(req, res) {
   try{
     let city = req.query.city;
-    let locationData = require('./data/location.json');
-    let getLocationObject = locationData[0];
-    let locationObject = new Location(city,getLocationObject.display_name,getLocationObject.lat,getLocationObject.lon);
-
-    res.send(locationObject);
-  } catch(error){
-    res.status(500).send('something went wrong ');
-  }
-}
-let weatherArr=[];
-function handelWeatherRequest(req, res) {
-  try{
+    const url= `https://us1.locationiq.com/v1/search.php?key=${geo_api_key}&q=${city}&format=json`;
+    // let locationData = require('./data/location.json');
 
 
-    if(weatherArr){
-      weatherArr=[];
-    }
-    let weatherData = require('./data/ weather.json');
-    let weather = weatherData.data;
-    weather.forEach(element =>{
-      new Weather(element.valid_date,element.weather.description);
-
+    superAgent.get(url).then(res =>{
+      let getLocationObject = res.body;
+      let locationObject = new Locations(city,getLocationObject.display_name,getLocationObject.lat,getLocationObject.lon);
+      response.send(locationObject);
     });
-    res.send(weatherArr);
-  }
-  catch(error){
-    res.status(500).send('something went wrong ');
 
+  } catch(error){
+    response.status(500).send('something went wrong ');
   }
 }
 
- function handelParkRequest(req, response) {
-     try{
-    const url =`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${parkApi}`;
-     superAgent.get(url).then( res => {  
-         const park=res.body.data;
-park.map(element =>{
-    const name=element.name;
-const address=element.address;
-const fee=element.fee;
-const description=element.description;
-    const url=element.url;
-     new Park(name,address,fee,description,url)
-
-})
-response.send(parkArr);
-
-          })
-          res.send(parkArr);
-        }
-      catch (error){
-        response.send(error);
-      };
-
-    }
-     let parkArr=[]
-  
-function Park(name,add,fee,des,url){
-    this.name=name;
-    this.address=add;
-    this.fee=fee;
-    this.description=des;
-    this.url=url;
-    parkArr.push(this);
-
-
-
-}
-// constructors
-
-function Location(search_query, formatted_query, latitude, longitude){
+function Locations(search_query, formatted_query, latitude, longitude){
   this.search_query= search_query;
   this. formatted_query= formatted_query;
   this.latitude= latitude;
   this.longitude = longitude;
 }
 
-function Weather(forecast,time){
-  this.forecast=forecast;
-  this.time = time;
-  weatherArr.push(this);
+let weatherArr=[];
+function handelWeatherRequest(req, response) {
 
-}
+  try{
+
+    if(weatherArr){
+      weatherArr=[];
+    }
+    // let weatherData = require('./data/ weather.json');
+    // let weather = weatherData.data;
+    const url= `https://api.weatherbit.io/v2.0/current/airquality?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&key=${weatherKey}`;
+
+    superAgent.get(url).then(res =>{
+      let getWe = res.body;
+      getWe.map(element =>{
+        return new Weather(getWe.valid_date,getWe.weather.description);
+
+      });
+
+      response.send(weatherArr);
+    });
+    //res.send(weatherArr);
+  }
+  catch(error){
+    response.status(500).send(error);
+  }
+
+  // function handelLocationRequest(req, res) {
+  //   try{
+  //     let city = req.query.city;
+  //     let locationData = require('./data/location.json');
+  //     let getLocationObject = locationData[0];
+  //     let locationObject = new Location(city,getLocationObject.display_name,getLocationObject.lat,getLocationObject.lon);
+
+  //     res.send(locationObject);
+  //   } catch(error){
+  //     res.status(500).send('something went wrong ');
+  //   }
+  // }
+  // let weatherArr=[];
+  // function handelWeatherRequest(req, res) {
+  //   try{
+
+
+  //     if(weatherArr){
+  //       weatherArr=[];
+  //     }
+  //     let weatherData = require('./data/ weather.json');
+  //     let weather = weatherData.data;
+  //     weather.forEach(element =>{
+  //       new Weather(element.valid_date,element.weather.description);
+
+  //     });
+  //     res.send(weatherArr);
+  //   }
+  //   catch(error){
+  //     res.status(500).send('something went wrong ');
+
+  //   }
+  // }
+
+  // function handelParkRequest(req, response) {
+  //   try{
+  //     const url =`https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=${parkApi}`;
+  //     superAgent.get(url).then( res => {
+  //       const park=res.body.data;
+  //       park.map(element =>{
+  //         const name=element.name;
+  //         const address=element.address;
+  //         const fee=element.fee;
+  //         const description=element.description;
+  //         const url=element.url;
+  //         new Park(name,address,fee,description,url);
+
+  //       });
+  //       response.send(parkArr);
+
+  //     });
+  //     response.send(parkArr);
+  //   }
+  //   catch (error){
+  //     response.send(error);
+  //   }
+
+  // }
+
+  // constructors
+
+
+  function Weather(forecast,time){
+    this.forecast=forecast;
+    this.time = time;
+    weatherArr.push(this);
 
   }
+
+}
 function notFoundHandler(request, response) {
   response.status(404).send('plz enter correct ^ _ ^');
 }
-  
+
 // app.use('*', (req, res) => {
 //     res.send('all good nothing to see here!');
 // });
